3b4fa529540332daa9602835b60131c940d07553
vedantswain <vedant12118@iiitd.ac.in>
2015-12-30 18:40:42 +0530

    added back to top

--- assets/js/my.js
+++ assets/js/my.js
@@ -3,3 +3,20 @@ $(document).ready(function(){
   	mode: 'fade',
   });
 });
+
+var amountScrolled = 300;
+
+$(window).scroll(function() {
+	if ( $(window).scrollTop() > amountScrolled ) {
+		$('a.back-to-top').fadeIn('slow');
+	} else {
+		$('a.back-to-top').fadeOut('slow');
+	}
+});
+
+$('a.back-to-top').click(function() {
+	$('html, body').animate({
+		scrollTop: 0
+	}, 700);
+	return false;
+});
