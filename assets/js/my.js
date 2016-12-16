aab2076f28432e667e1ec7c525177573fee09627
vedantswain <vedant12118@iiitd.ac.in>
2016-12-16 18:09:05 -0500

    WIP: Search tags working exclusively

--- assets/js/my.js
+++ assets/js/my.js
@@ -20,3 +20,28 @@ $('a.back-to-top').click(function() {
 	}, 700);
 	return false;
 });
+
+$('.label-default.tag').click(function(){
+	$('.label-default.tag').toggleClass("filtered");
+	filter= this.innerHTML;
+	console.log(filter);
+	postFilter(filter)
+});
+
+function postFilter(input) {
+    var filter, li, a, i;
+
+    filter=input.toUpperCase();
+    li = document.getElementsByClassName("mini-post");
+    
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        console.log(a.innerHTML);
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+
+        }
+    }
+}
