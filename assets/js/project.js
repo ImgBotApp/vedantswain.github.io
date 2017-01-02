dce4d00e13a8202f92ad696dd15febe9dc29e3f6
vedantswain <vedant12118@iiitd.ac.in>
2017-01-02 01:17:41 -0500

    WIP: Added all the content for reco

--- assets/js/project.js
+++ assets/js/project.js
@@ -7,3 +7,22 @@ $('.head-link').click(function(e) {
         scrollTop: $(goto).offset().top - 70
     }, 500);
 });
+
+// Get the modal
+var modal = document.getElementById('myModal');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+$('.project-thumb').click(function(e){
+    modal.style.display = "block";
+    modalImg.src = $(this).attr('src');
+    captionText.innerHTML = this.alt;
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
